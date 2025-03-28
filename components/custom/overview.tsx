2ec9a314d1887d5d0773c205ff319acd76f9e01b
Surakarapu Shanmukh Srinivas <139730408+theonlyshanmukh@users.noreply.github.com>
2025-03-28 15:14:47 +0000

    new

--- components/custom/overview.tsx
+++ components/custom/overview.tsx
@@ -19,30 +19,8 @@ export const Overview = () => {
           <span>+</span>
           <MessageIcon />
         </p>
-        <p>
-          This is an open source Chatbot template powered by the Google Gemini
-          model built with Next.js and the AI SDK by Vercel. It uses the{" "}
-          <code className="rounded-sm bg-muted-foreground/15 px-1.5 py-0.5">
-            streamText
-          </code>{" "}
-          function in the server and the{" "}
-          <code className="rounded-sm bg-muted-foreground/15 px-1.5 py-0.5">
-            useChat
-          </code>{" "}
-          hook on the client to create a seamless chat experience.
-        </p>
-        <p>
-          {" "}
-          You can learn more about the AI SDK by visiting the{" "}
-          <Link
-            className="text-blue-500 dark:text-blue-400"
-            href="https://sdk.vercel.ai/docs"
-            target="_blank"
-          >
-            Docs
-          </Link>
-          .
-        </p>
+        
+        
       </div>
     </motion.div>
   );
--- components/flights/authorize-payment.tsx
+++ components/flights/authorize-payment.tsx
@@ -74,7 +74,7 @@ export function AuthorizePayment({
         Use your saved information for this transaction
       </div>
       <div className="text-muted-foreground text-sm sm:text-base">
-        Enter the magic word to authorize payment. Hint: It rhymes with bercel.
+        Enter the magic word to authorize payment. 
       </div>
 
       <Input
